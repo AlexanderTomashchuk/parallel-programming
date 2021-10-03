83c4de9a2dc769fe6e013acf356e5e0862d5dca0
AlexanderTomashchuk <alexandertomashchuk@gmail.com>
2021-10-03 16:25:32 +0300

    difference between foreground and background threads

---
+++ richter-book-examples/threads/ForegroundVsBackgroundThreads/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace ForegroundVsBackgroundThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание нового потока (по умолчанию активного)
+            Thread t = new Thread(Worker);
+            
+            // Превращение потока в фоновый
+            t.IsBackground = true;
+            
+            t.Start(); // Старт потока
+            
+            // В случае активного потока приложение будет работать около 10 секунд
+            // В случае фонового потока приложение немедленно прекратит работу
+            Console.WriteLine("Returning from Main");
+        }
+
+        private static void Worker()
+        {
+            Thread.Sleep(10000); // Имитация 10 секунд работы
+            // Следующая строка выводится только для кода,
+            // исполняемого активным потоком
+            Console.WriteLine("Returning from Worker");
+        }
+    }
+}
